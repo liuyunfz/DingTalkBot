a09d0c051932a8ab843f3356954772ea1251e36b
liuyunfz <46259392+liuyunfz@users.noreply.github.com>
2020-07-26 15:44:32 +0800

    访问添加headers

--- run.py
+++ run.py
@@ -31,7 +31,23 @@ def sent_message(text:str,title:str,picUrl:str,messageUrl:str):
     print(rsp.json().get('errmsg'))
 def get_message():
     url="https://www.x6d.com/html/34.html"
-    rsp=requests.get(url)
+    headers={
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding':'gzip, deflate, br',
+    'Accept-Language':'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'Cache-Control':'max-age=0',
+    'Connection':'keep-alive',
+    'Cookie':'UM_distinctid=17106df295e9e8-0eb60fb0d4a571-f313f6d-1fa400-17106df295fa64; CNZZDATA1278516878=330133885-1587811355-%7C1595746446',
+    'Host':'www.x6d.com',
+    'Referer':'https://www.x6d.com/',
+    'Sec-Fetch-Dest':'document',
+    'Sec-Fetch-Mode':'navigate',
+    'Sec-Fetch-Site':'same-origin',
+    'Sec-Fetch-User':'?1',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+    }
+    rsp=requests.get(url=url,headers=headers)
     s=etree.HTML(rsp.text)
     print(rsp.text)
     s=s.xpath("//li[@class='layui-clear']")
