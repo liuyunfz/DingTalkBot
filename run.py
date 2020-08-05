ff384c32034525a00462dd1519b3d65a8664adae
liuyunfz <liuyunfz@gamil.com>
2020-08-05 21:25:35 +0800

    delete 28800

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
                 date=s.xpath("//time")[0].xpath('string(.)')        
                 timeArray = time.strptime(date+":00", "%Y-%m-%d %H:%M:%S")
                 timestamp = time.mktime(timeArray)
-                ac_time=utc_stp+28800-timestamp
+                ac_time=utc_stp-timestamp
                 #默认时间频率为两小时，单位秒即7200，可以根据自己需求更改。中国为东八区，比标准时间快8小时。
                 if ac_time<7200 :
                     sent_message(token=token,secret=secret,text=date+"\n"+info,title=title,picUrl=img,messageUrl=url)
@@ -74,8 +74,8 @@ if __name__ == "__main__":
                 video_list = get_video(i)
                 print(video_list)
                 for j in video_list:
-                    ac_time = utc_stp+28800 - j['created'] 
-                    print(utc_stp+28800,j['created'],ac_time)
+                    ac_time = utc_stp- j['created'] 
+                    print(utc_stp,j['created'],ac_time)
                     if ac_time<7200 :
                         sent_message(token=token,secret=secret,text=j['author']+"\n"+j['description'],title=j['title'],picUrl="https:{}".format(j['pic']),messageUrl="https://www.bilibili.com/video/{}".format(j['bvid']))
                         print("log:",j['created'],j['author'],j['title'],"\n")
