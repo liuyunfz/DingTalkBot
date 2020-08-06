aaea42b38a290925c5a0871f6fd671558e46d1c9
liuyunfz <liuyunfz@gamil.com>
2020-08-06 11:13:07 +0800

    修复时间代码，更改了消息格式

--- run.py
+++ run.py
@@ -38,16 +38,7 @@ if __name__ == "__main__":
             bili_subscribe = True
         except:
             bili_subscribe =  False
-        '''
-        from datetime import datetime
-        # 协调世界时
-        s=str(datetime.utcnow())[:19]
-        timeArray = time.strptime(s, "%Y-%m-%d %H:%M:%S")
-        timestamp = time.mktime(timeArray)
-        print(int(timestamp))
-        utc_stp=int(timestamp)
-        '''
-        utc_stp=int(time.time())
+        China_stp=int(time.time())#action获取的系统时间突然变成了utc+8，原因不明
         #小刀网线报处理
         datas=get_message() 
         try:     
@@ -58,8 +49,8 @@ if __name__ == "__main__":
                 date=s.xpath("//time")[0].xpath('string(.)')        
                 timeArray = time.strptime(date+":00", "%Y-%m-%d %H:%M:%S")
                 timestamp = time.mktime(timeArray)
-                ac_time=utc_stp-timestamp
-                #默认时间频率为两小时，单位秒即7200，可以根据自己需求更改。中国为东八区，比标准时间快8小时。
+                ac_time=China_stp-timestamp
+                #默认时间频率为两小时，单位秒即7200，可以根据自己需求更改。
                 if ac_time<7200 :
                     sent_message(token=token,secret=secret,text=date+"\n"+info,title=title,picUrl=img,messageUrl=url)
                     print("log:",date,title,info,"\n")
@@ -72,12 +63,14 @@ if __name__ == "__main__":
             mid_list = mids.split(',')
             for i in mid_list:
                 video_list = get_video(i)
-                print(video_list)
                 for j in video_list:
-                    ac_time = utc_stp- j['created'] 
-                    print(utc_stp,j['created'],ac_time)
+                    ac_time = China_stp- j['created'] 
+                    print(China_stp,j['created'],ac_time)
                     if ac_time<7200 :
-                        sent_message(token=token,secret=secret,text=j['author']+"\n"+j['description'],title=j['title'],picUrl="https:{}".format(j['pic']),messageUrl="https://www.bilibili.com/video/{}".format(j['bvid']))
+                        import datetime
+                        dateArray = datetime.datetime.fromtimestamp(j['created'])
+                        otherStyleTime = dateArray.strftime("%m-%d %H:%M:%S")
+                        sent_message(token=token,secret=secret,text=j['author']+" "+otherStyleTime+"\n"+j['description'],title=j['title'],picUrl="https:{}".format(j['pic']),messageUrl="https://www.bilibili.com/video/{}".format(j['bvid']))
                         print("log:",j['created'],j['author'],j['title'],"\n")
                     else:
                         break
