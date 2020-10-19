45fa97a532dbd4a7cefe3811f6cbf75dec558639
zhangyilin <891809266@qq.com>
2020-10-19 17:28:10 +0800

    ++++

---
+++ config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     config.py
+   Description :   
+   Author :        ZhangYiLin
+   date：          2020/10/19 17:18
+-------------------------------------------------
+   Change Activity:
+                   2020/10/19 17:18
+-------------------------------------------------
+"""
+secret = "SEC2a3e245211fea2bc09855d557d563aa9fee8d01ab0cbba6b48a75682cf30bd79"
+token = "c6312ea0afc593be4f6beb42694be9f5d1fb4a98f365d4d2618169c75ca56b6b"
+mids = "16794231"
